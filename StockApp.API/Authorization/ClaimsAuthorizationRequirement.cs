15f267bd84b408812d992423846ec20553a813e4
MateusLGoncalves <mateuslgoncalves@outlook.com>
2024-11-25 22:25:16 -0300

    Tarefa 62 - Implementar Controle de Acesso Baseado em Claims

---
+++ StockApp.API/Authorization/ClaimsAuthorizationRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+public class ClaimsAuthorizationRequirement : IAuthorizationRequirement
+{
+    public string ClaimType { get; }
+    public string ClaimValue { get; }
+
+    public ClaimsAuthorizationRequirement(string claimType, string claimValue)
+    {
+        ClaimType = claimType;
+        ClaimValue = claimValue;
+    }
+}
---
+++ StockApp.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class AuthController : Controller
+{
+    // POST /api/auth/login
+    [HttpPost("login")]
+    public async Task<IActionResult> Login()
+    {
+        // Criação de claims simuladas
+        var claims = new List<Claim>
+        {
+            new Claim("Permission", "CanManageProducts"),
+            new Claim(ClaimTypes.Name, "TestUser")
+        };
+
+        var identity = new ClaimsIdentity(claims, "CookieAuth");
+        var principal = new ClaimsPrincipal(identity);
+
+        // Salvar usuário no contexto HTTP
+        await HttpContext.SignInAsync("CookieAuth", principal);
+
+        return Ok(new { message = "Login realizado com sucesso!" });
+    }
+
+    // GET /api/auth/check-access
+    [Authorize(Policy = "CanManageProducts")]
+    [HttpGet("check-access")]
+    public IActionResult CheckAccess()
+    {
+        return Ok(new { message = "Você tem permissão para acessar este recurso!" });
+    }
+}
